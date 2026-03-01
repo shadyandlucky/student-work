75f424fbe3b80754afacdbb309833278d5978691
Dori Rivera <69157433+shadyandlucky@users.noreply.github.com>
2026-03-01 18:04:53 -0500

    Add files via upload

---
+++ js/core.js
@@ -0,0 +1,37 @@
+// JavaScript Document
+
+//Smooth Scroll (Works with jQuery Versionz 1.8 - 2.0.2)
+
+/*$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash,
+	    $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 1000, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+});*/
+
+//End Smooth Scroll
+
+//show/hide only 1 div at a time
+
+function showonlyone(thechosenone) {
+      var newboxes = document.getElementsByTagName("div");
+            for(var x=0; x<newboxes.length; x++) {
+                  name = newboxes[x].getAttribute("class");
+                  if (name == 'newboxes') {
+                        if (newboxes[x].id == thechosenone) {
+                        newboxes[x].style.display = 'block';
+                  }
+                  else {
+                        newboxes[x].style.display = 'none';
+                  }
+            }
+      }
+};
